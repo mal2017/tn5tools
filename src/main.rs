f7da43c3421c2d83804abca632d2ff40a9ca4764
Matt Lawlor <matt.a.lawlor@gmail.com>
2018-06-06 14:14:32 -0400

    added multithreading

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate clap;
 extern crate tn5tools;
 extern crate rust_htslib;
 extern crate rayon;
+extern crate indicatif;
 
 fn main() {
 	use clap::App;
@@ -15,29 +16,37 @@ fn main() {
         // Safe to use unwrap() because of the required() option
         let bams: Vec<&str> = m.values_of("BAMS").unwrap().collect();
     	let bed: &str = m.value_of("BED").unwrap();
-        counts(bed, &bams);
+    	let threads: u32 =  m.value_of("threads").unwrap_or("1").to_string().parse().unwrap();
+        counts(bed, &bams, threads as usize);
 	}
 
 }
 
 
-fn counts(bed_path: &str, bams: &Vec<&str>) {
+fn counts(bed_path: &str, bams: &Vec<&str>, p: usize) {
 	use rayon::prelude::*;
 	use rust_htslib::bam::IndexedReader;
 	use tn5tools::regions::get_reads_in_region;
 	use tn5tools::regions;
-
+	use indicatif::{ProgressBar, HumanDuration};
+	use std::sync::{Arc, Mutex};
+	use rayon::ThreadPoolBuilder;
 	let regions = regions::bed_as_strings(bed_path);
 
-	let n_regions = regions.len() as u32;
-
+	let n_regions = regions.len() as u64;
+	ThreadPoolBuilder::new().num_threads(p).build_global().unwrap();
 	for bam in bams {
+		let pb = Arc::new(Mutex::new(ProgressBar::new(n_regions)));
+
 		let counter = regions.par_iter()
-							 .map(|x| get_reads_in_region(bam, &x.0, &x.1, &x.2));
+							 .map(|x| {		 	
+							 	get_reads_in_region(bam, &x.0, &x.1, &x.2)
+							 });
 
-		let counts: Vec<u32> = counter.collect();     
+		let counts: Vec<u32> = counter.collect();
+		pb.lock().unwrap().finish_with_message("Done");   
     }
-
+    
 }
 
 
