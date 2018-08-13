ed8f024ca67941a010f87b84dbaf65c1caa957ae
mattlawlor <matt.a.lawlor@gmail.com>
2018-08-13 15:22:45 -0400

    fixed new aux data setting

--- src/shift.rs
+++ src/shift.rs
@@ -20,12 +20,14 @@ pub fn shift_bam(ib: &str, ob: &str, p: usize) {
 	// only initialize once + use while loop
 	let mut bam_rec = bam::Record::new();
 	let mut new_cigarstr = CigarString(vec![Cigar::Match(0)]);
-	let mut new_insize: i32;;
+	let mut new_insize: i32;
 	let mut is_rev: bool;
 	let mut seq: Vec<u8>;
 	let mut slen: usize;
 	let mut qual: Vec<u8>;
 	let mut qname: Vec<u8>;
+	let mut new_qual: Vec<u8>;
+	let mut new_seq: Vec<u8>;
 
 	while let Ok(_r) = bam.read(&mut bam_rec) {
 
@@ -35,22 +37,27 @@ pub fn shift_bam(ib: &str, ob: &str, p: usize) {
 		} else {
 			new_insize = bam_rec.insert_size() - 9
 		};
-		//println!("{:?}", new_insize);
-		//let mut cigar  = bam_rec.cigar();//.clone();
+
 		qual   = bam_rec.qual().to_owned();
-		qname   = bam_rec.qname().to_owned();
+		qname  = bam_rec.qname().to_owned();
 		seq    = bam_rec.seq().as_bytes();
-		slen   = bam_rec.seq().len();
-		new_cigarstr  = CigarString(vec![Cigar::Match(slen as u32)]);
-		
+		slen    = bam_rec.seq().len();
+
 		//cigar_utils::trim_cigar_string_tn5(&cigar, &is_rev);
 
 		// https://www.biostars.org/p/76892/
 		let new_seq: &[u8] = if is_rev {
-			let idx = seq.len() - 4;
+			let idx = slen - 4;
 			&seq[..idx]
 		} else {
 			&seq[5..]
+		};
+
+		let new_qual: &[u8] = if is_rev {
+			let idx = slen - 4;
+			&qual[..idx]
+		} else {
+			&qual[5..]
 		};
 
 		let pos = if is_rev {
@@ -59,10 +66,12 @@ pub fn shift_bam(ib: &str, ob: &str, p: usize) {
 			bam_rec.pos() + 4
 		} as u32;
 
+		new_cigarstr = CigarString(vec![Cigar::Match(new_seq.len() as u32)]);
+
 		bam_rec.set(&qname,
 					&new_cigarstr,
 					&new_seq,
-					&qual);
+					&new_qual);
 
 		// account for offsets with new insert size
 		bam_rec.set_insert_size(new_insize);
