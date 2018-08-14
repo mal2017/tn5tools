42f352018f6885a2953663f540de7b18b6539c21
mattlawlor <matt.a.lawlor@gmail.com>
2018-08-13 22:32:04 -0400

    switch shift to sam stdout vs bam

--- src/shift.rs
+++ src/shift.rs
@@ -1,21 +1,20 @@
 use rust_htslib::bam;
+use rust_htslib::sam;
 use rust_htslib::prelude::*;
 use std::str;
 use ::cigar_utils;
 
 // shifts all reads in a bam
 
-pub fn shift_bam(ib: &str, ob: &str, p: usize) {
+pub fn shift_bam(ib: &str) {
 	use std::ops::Deref;
-	use rust_htslib::bam::record::{Cigar, CigarString};
+	use rust_htslib::bam::record::{Cigar, CigarString, Aux};
 
 	let mut bam = bam::Reader::from_path(ib).unwrap();
 	let header = bam::Header::from_template(bam.header());
-	let mut obam = bam::Writer::from_path(ob, &header).unwrap();
 
-	if p > 1 {
-		obam.set_threads(p);
-	}
+	//let mut obam = bam::Writer::from_path(ob, &header).unwrap();
+	let mut osam = sam::Writer::from_stdout(&header).unwrap();
 
 	// only initialize once + use while loop
 	let mut bam_rec = bam::Record::new();
@@ -28,63 +27,46 @@ pub fn shift_bam(ib: &str, ob: &str, p: usize) {
 	let mut qname: Vec<u8>;
 	let mut new_qual: Vec<u8>;
 	let mut new_seq: Vec<u8>;
+	let mut pos: u32;
+	let mut idx: usize;
 
 	while let Ok(_r) = bam.read(&mut bam_rec) {
 
 		is_rev = bam_rec.is_reverse();
-		if is_rev {
-			new_insize = bam_rec.insert_size() + 9
-		} else {
-			new_insize = bam_rec.insert_size() - 9
-		};
-
 		qual   = bam_rec.qual().to_owned();
 		qname  = bam_rec.qname().to_owned();
 		seq    = bam_rec.seq().as_bytes();
-		slen    = bam_rec.seq().len();
+		slen   = bam_rec.seq().len();
 
 		//cigar_utils::trim_cigar_string_tn5(&cigar, &is_rev);
-
 		// https://www.biostars.org/p/76892/
-		let new_seq: &[u8] = if is_rev {
-			let idx = slen - 4;
-			&seq[..idx]
-		} else {
-			&seq[5..]
-		};
 
-		let new_qual: &[u8] = if is_rev {
-			let idx = slen - 4;
-			&qual[..idx]
+		if is_rev {
+			idx = slen - 4;
+			new_seq = seq[..idx].to_vec();
+			pos = bam_rec.pos() as u32 - 5;
+			new_qual = qual[..idx].to_vec();
+			new_insize = bam_rec.insert_size() + 9;
 		} else {
-			&qual[5..]
-		};
-
-		let pos = if is_rev {
-			bam_rec.pos() - 5
-		} else {
-			bam_rec.pos() + 4
-		} as u32;
+			pos = bam_rec.pos() as u32 + 4;
+			new_seq = seq[5..].to_vec();
+			new_qual = qual[5..].to_vec();
+			new_insize = bam_rec.insert_size() - 9;
+		}
 
 		new_cigarstr = CigarString(vec![Cigar::Match(new_seq.len() as u32)]);
-
-		bam_rec.set(&qname,
-					&new_cigarstr,
-					&new_seq,
-					&new_qual);
 
 		// account for offsets with new insert size
 		bam_rec.set_insert_size(new_insize);
 		
 		bam_rec.set_pos(pos as i32);
 
-		//bam_rec.set(&qname, &cigar, new_seq, &qual);
-		obam.write(&bam_rec).unwrap();
+		bam_rec.set(&qname,
+			&new_cigarstr,
+			&new_seq,
+			&new_qual);
+
+		osam.write(&bam_rec).unwrap();
 	}	
 
-}
-
-
-pub fn shift_read(r: bam::Record) {
-	// TODO: impl for record
 }
