a4b8436698439dc0995d525762a2f087b71563b0
Matt Lawlor <matt.a.lawlor@gmail.com>
2018-08-28 16:33:10 -0400

    beginnings of par iter, but but need to iron out coords

--- src/cigar_utils.rs
+++ src/cigar_utils.rs
@@ -1,7 +1,8 @@
 use rust_htslib::bam;
 use rust_htslib::prelude::*;
 use std::iter;
-
+use rust_htslib::bam::record::{Cigar, CigarString, Aux};
+use rayon::prelude::*;
 
 // 1. expand cigar
 // 2. consume cigar while following rules
@@ -15,24 +16,131 @@ fn expand_cigarstring(cs: &bam::record::CigarString) -> Vec<char> {
 }
 
 
-pub fn trim_cigar_string_tn5(cs: &bam::record::CigarString, is_rev: &bool) {
+// TODO char_vec_to_cigarstring
+fn char_vec_to_cigarstring(cv: Vec<char>) -> CigarString {
+	let mut current_char = 'z'; // dummy char
+	let mut current_ct = 1;
 
-	let cs_vec = expand_cigarstring(cs);
-	let upper_idx = cs_vec.len() - 1;
+	let mut cigar_vec: Vec<Cigar> = Vec::new();
 
-	let rng: Vec<usize> = if *is_rev {
-		(0..upper_idx).rev().collect()
+	for x in cv {
+		if current_char == 'z' {
+			current_char = x;
+		} else if current_char == x {
+			current_ct += 1;
+		} else {
+			cigar_vec.push(char_to_cigar(&current_char, current_ct).unwrap());
+			current_char = x;
+			current_ct = 1;
+		}
+		cigar_vec.push(char_to_cigar(&current_char, current_ct).unwrap()); // final cigar
+	}
+
+	CigarString(cigar_vec)
+}
+
+fn char_to_cigar(t: &char, n: u32) -> Option<Cigar> {
+	match t {
+		'M' => Some(Cigar::Match(n)),
+        'I' => Some(Cigar::Ins(n)),
+        'D' => Some(Cigar::Del(n)),
+        'N' => Some(Cigar::RefSkip(n)),
+        'H' => Some(Cigar::HardClip(n)),
+        'S' => Some(Cigar::SoftClip(n)),
+        'P' => Some(Cigar::Pad(n)),
+        '=' => Some(Cigar::Equal(n)),
+        'X' => Some(Cigar::Diff(n)),
+        _ => {
+        	println!("{:?}", t);
+        	None
+        },
+	}
+}
+
+pub fn trim_cigar_adjust_shift_tn5(cs: &bam::record::CigarString, is_rev: &bool) -> CigarTrimParams {
+
+	// current cigar string as char vec
+	let cs_vec: Vec<char> = if *is_rev {
+		expand_cigarstring(cs).into_iter().rev().collect()
 	} else {
-		(0..upper_idx).collect()
+		expand_cigarstring(cs)
 	};
 
+	// counter for remaining moves through the cigar string
+	let mut moves = if *is_rev { 
+		5
+	} else {
+		4
+	};
+	// tracker for adjusted shift count for the sequence (returned by this func)
+	let mut seq_shift = 0; 
+	// tracker for adjusted shift count for the reference (returned by this func)
+	let mut pos_shift = 0;
+
+	// index tracker for iterating through cigar string
+	let mut idx = 0;
 	let mut cgr: char;
-	for i in rng {
-		cgr = cs_vec[i];
+
+	// KEEP FOR DEBUG
+	//let mut has_indel: bool = false;
+
+	while (moves > 0) & ((cs_vec[idx] != 'D') & (cs_vec[idx] != 'N') & (cs_vec[idx] != 'H')) {
+		cgr = cs_vec[idx];
 		match cgr {
-			'M' => (),
-			'D' => (),
-			_   => (),
+			'M' | '=' | 'X' => {
+				moves -= 1;
+				seq_shift += 1;
+				pos_shift += 1;
+				//has_indel =false;
+			},
+			'D' | 'N' | 'H' | 'P' => {
+				// moves stay the same, essentially ignoring dels for the purpose of shifting
+				// seq_shift stays the same, because we ignored dels
+				pos_shift += 1; // position shift moves so we can pick back up on the other side of the del
+				//has_indel = true;
+			},
+			'I' | 'S' => {
+				moves -= 1;
+				seq_shift +=1;
+				// pos shift remains, we're moving through an insertion not in ref
+				//has_indel = true
+			},
+			_ => {
+				moves -= 1;
+				seq_shift += 1;
+				pos_shift += 1;
+			},
 		}
+		idx += 1;
+		
 	}
+
+	let mut new_cs_vec: Vec<char> = cs_vec[idx..].to_owned();
+
+	if *is_rev {
+		new_cs_vec = new_cs_vec.into_iter().rev().collect();
+	}
+
+	let new_cigar_string = char_vec_to_cigarstring(new_cs_vec);
+	
+	// KEEP FOR DEBUG
+	//if has_indel {
+	//	println!("is rev:{:?} ---- seq shift:{:?} ---- pos shift:{:?} ---- cigarstr:{:?}",is_rev, seq_shift,pos_shift,cs);
+	//}
+	
+
+	CigarTrimParams {
+		cigar: new_cigar_string,
+		seq_shift: seq_shift,
+		pos_shift: pos_shift,
+	}
+
+	//CigarString(vec![Cigar::Match(35 as u32)])
+}
+
+
+pub struct CigarTrimParams {
+	pub cigar: CigarString,
+	pub seq_shift: u32,
+	pub pos_shift: u32,
 }
--- src/shift.rs
+++ src/shift.rs
@@ -6,19 +6,39 @@ use std::str;
 use ::cigar_utils;
 use ::header_utils;
 use linear_map::LinearMap;
+use rayon::iter::ParallelBridge;
+use rayon::prelude::*;
+use rayon::ThreadPoolBuilder;
+use std::sync::{Arc, Mutex};
+
 
 // shifts all reads in a bam
 
 pub fn shift_bam(ib: &str, ob: &str, p: usize) {
+
+	ThreadPoolBuilder::new().num_threads(p).build_global().unwrap();
+
 	use std::ops::Deref;
 	use rust_htslib::bam::record::{Cigar, CigarString, Aux};
 	let mut bam = bam::Reader::from_path(ib).unwrap();
-	bam.set_threads(p);
-	let mut header = header_utils::edit_hdr_unsrt(bam.header());
+
+	let header = header_utils::edit_hdr_unsrt(bam.header());
+
 	let mut obam = bam::Writer::from_path(ob, &header).unwrap();
+	
 	// only initialize once + use while loop
 	let mut bam_rec = bam::Record::new();
-	let mut new_cigarstr = CigarString(vec![Cigar::Match(0)]);
+
+	let mut new_cigarstr = cigar_utils::CigarTrimParams {
+		cigar: CigarString(vec![Cigar::Match(0)]),
+		seq_shift: 0,
+		pos_shift: 0,
+	};
+	let mut m_new_cigarstr = cigar_utils::CigarTrimParams {
+		cigar: CigarString(vec![Cigar::Match(0)]),
+		seq_shift: 0,
+		pos_shift: 0,
+	};
 	let mut new_insize: i32;
 	let mut is_rev: bool;
 	let mut seq: Vec<u8>;
@@ -31,46 +51,69 @@ pub fn shift_bam(ib: &str, ob: &str, p: usize) {
 	let mut idx: usize;
 	let mut mpos: i32;
 	let mut bin: u16;
-	while let Ok(_r) = bam.read(&mut bam_rec) {
-		// TODO: do i need to let/reassign here?
-		// TODO: if is not mapped, write record and skip
+
+	let mut shift_var: u32;
+
+
+	let a = bam.records().into_iter().par_bridge();
+
+	a.map(|k| () ).collect::<()>();
+
+	
+	/*while let Ok(_r) = bam.read(&mut bam_rec) {
+		// todo parallize with rayon
+		// TODO: avoid error if unpaired/untagged
+		if bam_rec.is_unmapped() {
+			obam.write(&bam_rec).unwrap();
+			continue;
+		}
+
 	 	is_rev = bam_rec.is_reverse();
 	 	qual   = bam_rec.qual().to_owned();
 	 	qname  = bam_rec.qname().to_owned();
 	 	seq    = bam_rec.seq().as_bytes();
 	 	slen   = bam_rec.seq().len();
+	 	new_insize = bam_rec.insert_size() - (m_new_cigarstr.seq_shift + new_cigarstr.seq_shift) as i32;
+
+	 	m_new_cigarstr = cigar_utils::trim_cigar_adjust_shift_tn5(&CigarString::from_bytes(bam_rec.aux(b"MC")
+	 																							 .unwrap()
+	 																							 .string()).unwrap(),
+	 		&!is_rev);
+
+
+		//TODO delete this
+	 	//new_cigarstr = CigarString(vec![Cigar::Match(new_seq.len() as u32)]); // this will safely call all matches
+	 	new_cigarstr = cigar_utils::trim_cigar_adjust_shift_tn5(&bam_rec.cigar(), &is_rev);
+	 	
+
 		// https://www.biostars.org/p/76892/
 		// https://jef.works/blog/2017/03/28/CIGAR-strings-for-dummies/
 	 	if is_rev {
-	 		idx = slen - 4;
+	 		idx = slen - new_cigarstr.seq_shift as usize;
 	 		new_seq = seq[..idx].to_vec();
-	 		pos = bam_rec.pos() as i32 - 5;
+	 		pos = bam_rec.pos() - new_cigarstr.pos_shift as i32;
 	 		new_qual = qual[..idx].to_vec();
-	 		new_insize = bam_rec.insert_size() + 9;
-	 		mpos = bam_rec.mpos() as i32 + 4;
+	 		mpos = bam_rec.mpos() + new_cigarstr.pos_shift as i32;
 	 	} else {
-	 		pos = bam_rec.pos() as i32 + 4;
-	 		new_seq = seq[5..].to_vec();
-	 		new_qual = qual[5..].to_vec();
-	 		new_insize = bam_rec.insert_size() - 9;
-	 		mpos = bam_rec.mpos() as i32 - 5;
+	 		pos = bam_rec.pos() + new_cigarstr.pos_shift as i32;
+	 		new_seq = seq[(new_cigarstr.pos_shift as usize +1 )..].to_vec();
+	 		new_qual = qual[(new_cigarstr.pos_shift as usize +1 )..].to_vec();
+	 		mpos = bam_rec.mpos() - m_new_cigarstr.pos_shift as i32;
 	 	}
+
 	 	bin = reg2bin(pos, pos + new_seq.len() as i32);
 	 	bam_rec.set_bin(bin);
-	 	//TODO delete this
-	 	new_cigarstr = CigarString(vec![Cigar::Match(new_seq.len() as u32)]);
-	 	// account for offsets with new insert size
-	 	cigar_utils::trim_cigar_string_tn5(&bam_rec.cigar(), &is_rev);
+
 
 	 	bam_rec.set_insert_size(new_insize);
 	 	bam_rec.set_pos(pos);
 	 	bam_rec.set_mpos(mpos);
 	 	bam_rec.set(&qname,
-	 		&new_cigarstr,
+	 		&new_cigarstr.cigar,
 	 		&new_seq,
 	 		&new_qual);
 		obam.write(&bam_rec).unwrap();
-	}	
+	}*/
 }
 
 // For details, see SAM V1 format spec.
