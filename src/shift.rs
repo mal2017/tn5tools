2ba6f5323d686ab1a7f02f34b5184fec67d5862a
Matt Lawlor <matt.a.lawlor@gmail.com>
2018-08-28 19:42:01 -0400

    fastest so far - need to fix coordinates

--- src/shift.rs
+++ src/shift.rs
@@ -15,9 +15,7 @@ use std::sync::{Arc, Mutex};
 // shifts all reads in a bam
 
 pub fn shift_bam(ib: &str, ob: &str, p: usize) {
-
-	ThreadPoolBuilder::new().num_threads(p).build_global().unwrap();
-
+ 
 	use std::ops::Deref;
 	use rust_htslib::bam::record::{Cigar, CigarString, Aux};
 	let mut bam = bam::Reader::from_path(ib).unwrap();
@@ -25,66 +23,53 @@ pub fn shift_bam(ib: &str, ob: &str, p: usize) {
 	let header = header_utils::edit_hdr_unsrt(bam.header());
 
 	let mut obam = bam::Writer::from_path(ob, &header).unwrap();
-	
-	// only initialize once + use while loop
+
 	let mut bam_rec = bam::Record::new();
 
-	let mut new_cigarstr = cigar_utils::CigarTrimParams {
-		cigar: CigarString(vec![Cigar::Match(0)]),
-		seq_shift: 0,
-		pos_shift: 0,
-	};
-	let mut m_new_cigarstr = cigar_utils::CigarTrimParams {
-		cigar: CigarString(vec![Cigar::Match(0)]),
-		seq_shift: 0,
-		pos_shift: 0,
-	};
-	let mut new_insize: i32;
+	if p >= 2 {
+		let p2 = if ((p % 2) == 0 ){
+			p / 2
+		} else {
+			(p - 1) / 2
+		};
+		bam.set_threads(p2);
+		obam.set_threads(p-p2);
+	} else {
+		bam.set_threads(1);
+		obam.set_threads(1);
+	}
+
 	let mut is_rev: bool;
-	let mut seq: Vec<u8>;
-	let mut slen: usize;
-	let mut qual: Vec<u8>;
-	let mut qname: Vec<u8>;
-	let mut new_qual: Vec<u8>;
-	let mut new_seq: Vec<u8>;
-	let mut pos: i32;
-	let mut idx: usize;
-	let mut mpos: i32;
-	let mut bin: u16;
-
-	let mut shift_var: u32;
-
-
-	let a = bam.records().into_iter().par_bridge();
-
-	a.map(|k| () ).collect::<()>();
-
+	let mut m_new_cigarstr;
+	let mut new_cigarstr;
+	let mut qual;
+	let mut qname;
+	let mut seq;
+	let mut slen;
+	let mut new_insize;
+	let mut new_qual;
+	let mut new_seq;
+	let mut pos;
+	let mut idx;
+	let mut mpos;
+	let mut bin;
 	
-	/*while let Ok(_r) = bam.read(&mut bam_rec) {
-		// todo parallize with rayon
-		// TODO: avoid error if unpaired/untagged
-		if bam_rec.is_unmapped() {
-			obam.write(&bam_rec).unwrap();
-			continue;
-		}
+	while let Ok(_r) = bam.read(&mut bam_rec) {
+		// TODO: throw out unmapped
 
-	 	is_rev = bam_rec.is_reverse();
+		is_rev = bam_rec.is_reverse();
+
+		m_new_cigarstr = cigar_utils::trim_cigar_adjust_shift_tn5(&CigarString::from_bytes(bam_rec.aux(b"MC")
+	 																							 .unwrap()
+	 																							 .string()).unwrap(), &!is_rev);
+
+	 	new_cigarstr = cigar_utils::trim_cigar_adjust_shift_tn5(&bam_rec.cigar(), &is_rev);
+
 	 	qual   = bam_rec.qual().to_owned();
 	 	qname  = bam_rec.qname().to_owned();
 	 	seq    = bam_rec.seq().as_bytes();
 	 	slen   = bam_rec.seq().len();
 	 	new_insize = bam_rec.insert_size() - (m_new_cigarstr.seq_shift + new_cigarstr.seq_shift) as i32;
-
-	 	m_new_cigarstr = cigar_utils::trim_cigar_adjust_shift_tn5(&CigarString::from_bytes(bam_rec.aux(b"MC")
-	 																							 .unwrap()
-	 																							 .string()).unwrap(),
-	 		&!is_rev);
-
-
-		//TODO delete this
-	 	//new_cigarstr = CigarString(vec![Cigar::Match(new_seq.len() as u32)]); // this will safely call all matches
-	 	new_cigarstr = cigar_utils::trim_cigar_adjust_shift_tn5(&bam_rec.cigar(), &is_rev);
-	 	
 
 		// https://www.biostars.org/p/76892/
 		// https://jef.works/blog/2017/03/28/CIGAR-strings-for-dummies/
@@ -102,18 +87,18 @@ pub fn shift_bam(ib: &str, ob: &str, p: usize) {
 	 	}
 
 	 	bin = reg2bin(pos, pos + new_seq.len() as i32);
+
 	 	bam_rec.set_bin(bin);
-
-
-	 	bam_rec.set_insert_size(new_insize);
+		bam_rec.set_insert_size(new_insize);
 	 	bam_rec.set_pos(pos);
 	 	bam_rec.set_mpos(mpos);
 	 	bam_rec.set(&qname,
 	 		&new_cigarstr.cigar,
 	 		&new_seq,
 	 		&new_qual);
+
 		obam.write(&bam_rec).unwrap();
-	}*/
+	}
 }
 
 // For details, see SAM V1 format spec.
@@ -136,6 +121,7 @@ fn reg2bin(beg: i32, e: i32) -> u16 {
 		return(((1<<3)-1)/7 + (beg>>26) as u16)
 	};
 	return(0u16)
+	
 }
 
 
